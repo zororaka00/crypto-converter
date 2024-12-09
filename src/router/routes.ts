eaec00d13501e55cecfa2742379b77c9aa9bccbd
Zoro Raka <rakawidhiantoro@gmail.com>
2024-12-09 22:36:11 +0700

    set redirect if to another pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -31,7 +31,7 @@ const routes: RouteRecordRaw[] = [
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    redirect: '/'
   },
 ];
 
